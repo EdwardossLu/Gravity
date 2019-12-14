88518aeb6ec36f6c17d1d68e20309ac0ab680088
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2019-12-14 13:56:27 +1100

    Created a LazerBeam Script

---
+++ Assets/Scripts/LazerBeam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class LazerBeam : MonoBehaviour
+{
+
+    private bool _switchLazerState = true;
+    
+    private Renderer _rend;
+    private Collider _col;
+
+
+    private void Awake() 
+    {
+        _rend = GetComponent<Renderer>();
+        _col = GetComponent<Collider>();
+    }
+
+    private void Start() 
+    {
+        StartCoroutine("SetBeamVisability");
+    }
+
+    private IEnumerator SetBeamVisability()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            BeamToggle();
+        }
+    }
+
+    private void BeamToggle()
+    {
+        _switchLazerState = !_switchLazerState;
+
+        _rend.enabled = _switchLazerState;
+        _col.enabled = _switchLazerState;
+    }
+}
