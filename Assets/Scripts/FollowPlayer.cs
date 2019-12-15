de93bcab6978ef2416ed83f8e5a6e0e3b7b77c74
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2019-12-16 00:26:09 +1100

    Created a FollowCamera script

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField]private Transform target = null;
+    [Range(0.01f, 0.1f)] [SerializeField] private float smoothFactor = 0f;
+
+    private Vector3 _cameraOffest;
+
+
+    private void Awake()
+    {
+        Assert.IsNotNull(target);
+    }
+
+	private void Start() 
+    {
+        // Get the distance between the camera and target.
+		_cameraOffest = transform.position - target.position;
+	}
+
+	void Update () 
+    {
+        // Set the distance between the camera and target.
+		Vector3 newPos = target.position + _cameraOffest;
+		transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+	}
+}
