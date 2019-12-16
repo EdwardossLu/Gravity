2b350053ff4cb5fdf87509168069c443ad2ddbc2
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2019-12-16 19:03:36 +1100

    Second pass for the PathSystem

--- Assets/Scripts/PathSystem.cs
+++ Assets/Scripts/PathSystem.cs
@@ -7,15 +7,50 @@ public class PathSystem : MonoBehaviour
     [Header("General")]
 
     [Tooltip("How fast the object should move between waypoints.")]
-    [Range(0.001f, 1f)] [SerializeField] private float lerpSpeed = 1f;
+    [Range(0.1f, 10f)] [SerializeField] private float speed = 1f;
 
     [Tooltip("List of waypoints for the object to move towards.")]
     [SerializeField] private List<Transform> waypoints = new List<Transform>();
+
+    private int _point = 0;
+    private bool _waypointDirection = false;
     
+
+    private void Awake() 
+    {
+        if (_point != 0) _point = 0;
+    }
 
     private void Update() 
     {
-        transform.position = Vector3.Lerp(transform.position, waypoints[1].position, .01f);
+        MoveToNewWaypoint();
+    }
+
+    private void MoveToNewWaypoint()
+    {
+        /*TODO:  Make this system move dynamic for future use.
+                       Remove hard coded values. */ 
+                       
+        float waypointMoveSpeed = speed * Time.deltaTime;
+
+        if (!_waypointDirection)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[1].position, waypointMoveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, waypoints[1].position) < 0.001f)
+            {
+                _waypointDirection = true;
+            }
+        }
+        else if (_waypointDirection)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[0].position, waypointMoveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, waypoints[0].position) < 0.001f)
+            {
+                _waypointDirection = false;
+            }
+        }
     }
 
     private void OnDrawGizmos() 
